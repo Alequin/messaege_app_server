d3c578af13d38717dd7a1e80806cb50db28e1018
James Cox <j-cox03@hotmail.com>
2017-08-26 20:29:38 +0100

    Changed bd connection method to pools

--- index.js
+++ index.js
@@ -29,15 +29,23 @@ app.get('/times', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  // https://node-postgres.com/guides/upgrading
+  var pool = new pg.Pool()
+
+  // connection using created pool
+  pool.connect(function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+      if (err){
+        console.error(err); response.send("Error " + err);
+      }else {
+        response.render('pages/db', {results: result.rows} );
+      }
     });
   });
+
+  // pool shutdown
+  pool.end()
 });
 
 app.listen(app.get('port'), function() {
