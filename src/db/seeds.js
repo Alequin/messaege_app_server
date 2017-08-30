81424c13ce6e0ce8385fa8fe529d47b3ef389975
James Cox <j-cox03@hotmail.com>
2017-08-30 21:11:44 +0100

    Used set timeout in seed to manage timing better

--- src/db/seeds.js
+++ src/db/seeds.js
@@ -1,11 +1,33 @@
 const Conversation = require("./../models/conversation");
 const User = require("./../models/user");
 
+let prepareTimeToWait = function(){
+  let timeToWait = 250;
+  return function(){
+    let toReturn = timeToWait;
+    timeToWait += 250;
+    return timeToWait;
+  }
+}
+let timeToWait = prepareTimeToWait();
+
+
 User.deleteAll();
 Conversation.deleteAll();
 
 const convo1 = new Conversation("2017-01-01");
-convo1.save();
+
+setTimeout(() => {
+  convo1.save();
+}, timeToWait());
 
 const user1 = new User("cool Name", 5, "android", "sfhfee", "online", true);
-user1.save();
+
+setTimeout(() => {
+  user1.save();
+}, timeToWait());
+
+setTimeout(() => {
+  User.findAll();
+  Conversation.findAll();
+}, timeToWait());
