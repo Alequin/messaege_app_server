f80c30868f690ca239229208f0ee1a83da32c9a8
James Cox <j-cox03@hotmail.com>
2017-08-30 20:36:09 +0100

    Moved tables.sql into src folder

--- src/db/seeds.js
+++ src/db/seeds.js
@@ -1,6 +1,10 @@
 const Conversation = require("./../models/conversation");
+const User = require("./../models/user");
 
 Conversation.deleteAll();
 
 const convo1 = new Conversation("2017-01-01");
 convo1.save();
+
+const user1 = new User("cool Name", 5, "android", "sfhfee", "online", true);
+user1.save();
