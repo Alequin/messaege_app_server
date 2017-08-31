99594611b30dbf61ba2cff0464cdf60e1e45ddf8
James Cox <j-cox03@hotmail.com>
2017-08-31 21:24:38 +0100

    Made messages controller

---
+++ controllers/messages.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const messageRouter = new express.Router();
+const requestAuth = require("./../src/services/request_auth");
+
+const Messages = require("./../src/models/message");
+
+messageRouter.get('/', requestAuth, function(req, res, next){
+  const onError = (error) => {console.log(error.stack)}
+  Messages.all(onError, (results) => {
+    res.json(results);
+  });
+});
+
+messageRouter.get('/conversation/:id', requestAuth, function(req, res, next){
+  const onError = (error) => {console.log(error.stack)}
+  const convoId = req.params.id;
+  Messages.getAllFromConversation(convoId, onError, (results) => {
+    res.json(results);
+  });
+});
+
+module.exports = messageRouter;
