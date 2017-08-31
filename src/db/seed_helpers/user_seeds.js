9159286d7b94bc6dfbc54ef6dbd2b16745b21f34
James Cox <j-cox03@hotmail.com>
2017-08-31 18:34:02 +0100

    added user to seeds

--- src/db/seed_helpers/user_seeds.js
+++ src/db/seed_helpers/user_seeds.js
@@ -4,6 +4,7 @@ function buildUsers(array){
   array.push(new User("cool Name", 5, "android", "sfhfee", "online", true));
   array.push(new User("awesome popper", 5, "android", "sfhfee", "online", true));
   array.push(new User("wow fan", 5, "android", "sfhfee", "online", true));
+  array.push(new User("cam pan", 5, "android", "sfhfee", "offline", true));
 
   let arr = [];
   for(let user of array) arr.push(user.save());
