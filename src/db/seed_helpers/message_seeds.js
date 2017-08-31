a9d527d09c5322ab3b67e3e473c3033b4be965c0
James Cox <j-cox03@hotmail.com>
2017-08-31 14:05:48 +0100

    Made function to build seeded messages in table

--- src/db/seed_helpers/message_seeds.js
+++ src/db/seed_helpers/message_seeds.js
@@ -1,15 +1,20 @@
 const Message = require("./../../models/message");
 const Participant = require("./../../models/participant");
+const Conversation = require("./../../models/conversation");
 
-function buildMessages(array, users, convos){
+function buildMessages(array, convos){
 
-  // const onError = (error) => {
-  //   console.log(error.stack)
-  // }
-  //
-  // convos.findParticipantsOf(convos[0].id, onError, (results) => {
-  //   const users =
-  // })
+  const onError = (error) => {
+    console.log(error.stack)
+  }
+
+  const convoId = convos[0].id
+  Conversation.findUsersOf(convoId, onError, (users) => {
+    array.push(new Message("hello", users[0].id, convoId, "2017-08-31 14:00:00.123456+00"));
+    array.push(new Message("how are you?", users[1].id, convoId, "2017-08-31 14:00:30.123456+00"));
+    array.push(new Message("im fine thanks", users[0].id, convoId, "2017-08-31 14:01:00.123456+00"));
+    for(var message of array) message.save();
+  })
 
 }
 
