e6a76a8191754a8eb3df6b3dd44c6078ce7cecb9
James Cox <j-cox03@hotmail.com>
2017-08-31 09:55:51 +0100

    Added more seed users and made participant seed helper

---
+++ src/db/seed_helpers/participant_seeds.js
@@ -0,0 +1,9 @@
+const Participant = require("./../../models/participant");
+
+function buildParticipants(array, users){
+
+  array.push();
+
+}
+
+module.exports = buildParticipants;
--- src/db/seed_helpers/user_seeds.js
+++ src/db/seed_helpers/user_seeds.js
@@ -2,6 +2,8 @@ const User = require("./../../models/user");
 
 function buildUsers(array){
   array.push(new User("cool Name", 5, "android", "sfhfee", "online", true));
+  array.push(new User("awesome popper", 5, "android", "sfhfee", "online", true));
+  array.push(new User("wow fan", 5, "android", "sfhfee", "online", true));
 
   for(let user of array) user.save();
 }
