48fc38262daaa257109c0b6dbbb3994e27ff01df
James Cox <j-cox03@hotmail.com>
2017-08-31 16:49:26 +0100

    removed time out calls on table logger

--- src/db/seed_helpers/table_logger.js
+++ src/db/seed_helpers/table_logger.js
@@ -4,26 +4,17 @@ const Participant = require("./../../models/participant");
 const Message = require("./../../models/message");
 
 const onError = (err) => {console.log("error")}
-let timeToWait = require("./wait_time_counter")(250);
 
 module.exports = function(){
-  setTimeout(() => {
     const onSuccessConversation = (results) => {console.log("Returned conversations: ", results.rows)}
     Conversation.all(onError, onSuccessConversation);
-  }, timeToWait());
 
-  setTimeout(() => {
-    const onSuccessUsers = (results) => {console.log("Returned Users: ", results.rows)}
+    const onSuccessUsers = (results) => {console.log("Returned Users: ", results)}
     User.all(onError, onSuccessUsers);
-  }, timeToWait());
 
-  setTimeout(() => {
     const onSuccessParticipants = (results) => {console.log("Returned participants: ", results.rows)}
     Participant.all(onError, onSuccessParticipants);
-  }, timeToWait());
 
-  setTimeout(() => {
     const onSuccessMessage = (results) => {console.log("Returned messages: ", results.rows)}
     Message.all(onError, onSuccessMessage);
-  }, timeToWait());
 }
