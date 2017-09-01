dc884c3a63f2f080eb1eeb7d84ccd197eb0e8ddf
James Cox <j-cox03@hotmail.com>
2017-09-01 22:24:13 +0100

    added updated method of using body parser

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 app.use(require("./controllers/index.js"));
 
 app.listen(app.get('port'), function() {
