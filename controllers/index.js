5582e5a85a4c48688a619c61a3409cfdbf398f42
James Cox <j-cox03@hotmail.com>
2017-09-01 14:34:42 +0100

    uncommented notification object

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const requestAuth = require("./../src/services/request_auth");
 
-// const Notification = require("./../src/services/notification");
+const Notification = require("./../src/services/notification");
 
 router.use("/users", require("./user"));
 router.use("/conversations", require("./conversation"));
@@ -13,13 +13,13 @@ router.get('/', requestAuth, function(req, res, next){
 });
 
 router.get('/test', requestAuth, function(req, res, next){
-  // var token = "cDQhiF_mOJw:APA91bF9Tg-GmtjNPCNBjVfC08a6y5QCgJuc50RO12KEuj55x9HuqXvDV8CYCaLySi-1ZJCVW_ejBtiHyoG8Tq8wdXRUxR5YFyjB-tZ9P1hdhwvbkOMp-tAl93kkj2jMctNN3hOgYW4f";
-  // var notification = {
-  //   title: 'Title of your push notification',
-  //   body: 'Body of your push notification'
-  // }
-  // var x = new Notification(token, notification);
-  // x.send();
+  var token = "cDQhiF_mOJw:APA91bF9Tg-GmtjNPCNBjVfC08a6y5QCgJuc50RO12KEuj55x9HuqXvDV8CYCaLySi-1ZJCVW_ejBtiHyoG8Tq8wdXRUxR5YFyjB-tZ9P1hdhwvbkOMp-tAl93kkj2jMctNN3hOgYW4f";
+  var notification = {
+    title: 'Title of your push notification',
+    body: 'Body of your push notification'
+  }
+  var x = new Notification(token, notification);
+  x.send();
   res.json({result: "sent"})
 });
 
