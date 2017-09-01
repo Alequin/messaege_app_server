b801adb2a3968017410a13aa5e21e721beee94db
James Cox <j-cox03@hotmail.com>
2017-09-01 10:00:09 +0100

    messages controller: removed needless next param and added empty post route "/"

--- controllers/messages.js
+++ controllers/messages.js
@@ -4,14 +4,18 @@ const requestAuth = require("./../src/services/request_auth");
 
 const Messages = require("./../src/models/message");
 
-messageRouter.get('/', requestAuth, function(req, res, next){
+messageRouter.get('/', requestAuth, function(req, res){
   const onError = (error) => {console.log(error.stack)}
   Messages.all(onError, (results) => {
     res.json(results);
   });
 });
 
-messageRouter.get('/conversation/:id', requestAuth, function(req, res, next){
+messageRouter.post('/', requestAuth, function(req, res){
+  
+});
+
+messageRouter.get('/conversation/:id', requestAuth, function(req, res){
   const onError = (error) => {console.log(error.stack)}
   const convoId = req.params.id;
   Messages.getAllFromConversation(convoId, onError, (results) => {
