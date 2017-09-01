93845108d82dc5e56e9ee6bbf822ed500dc25a03
James Cox <j-cox03@hotmail.com>
2017-09-01 13:43:37 +0100

    Made object to manage sending notifications

---
+++ src/services/notification.js
@@ -0,0 +1,26 @@
+
+var FCM = require('fcm-node');
+var serverKey = process.env.FIREBASE_KEY; //put the generated private key path here
+var fcm = new FCM(serverKey);
+
+function Notification(userToken, notification){
+  this.userToken = userToken;
+  this.notification = notification;
+}
+
+Notification.prototype.send = function(){
+  var note = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+    to: this.userToken,
+    notification: this.notification
+  }
+
+  fcm.send(note, function(err, response){
+    if (err) {
+      response.send("Something has gone wrong!");
+    } else {
+      response.send("Successfully sent with response: " + response);
+    }
+  });
+}
+
+module.exports = Notification;
