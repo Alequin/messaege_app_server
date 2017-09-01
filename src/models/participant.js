991451d7da254ecbc7f6dc861c7980e8ccf64b4a
James Cox <j-cox03@hotmail.com>
2017-09-01 10:23:19 +0100

    used tables.js in participants model

--- src/models/participant.js
+++ src/models/participant.js
@@ -1,5 +1,6 @@
 
 const SQL = require("./../db/sql_connection");
+const TABLES = require("./../db/tables");
 
 function Participant(userId, conversationId){
   this.id = -1;
@@ -7,12 +8,10 @@ function Participant(userId, conversationId){
   this.conversationId = conversationId;
 }
 
-Participant.tableName = "participants";
-
 Participant.prototype.save = function(){
 
   const sql = {
-    command: `INSERT INTO ${Participant.tableName}
+    command: `INSERT INTO ${TABLES.participant}
     (user_id, conversation_id)
     VALUES ($1, $2) RETURNING id;`,
     values: [this.userId,this.conversationId]
@@ -36,12 +35,12 @@ Participant.map = function(options){
 }
 
 Participant.all = function(onError, onSuccess){
-  const sql = {command: `SELECT * FROM ${Participant.tableName};`}
+  const sql = {command: `SELECT * FROM ${TABLES.participant};`}
   SQL.connect(sql, onError, onSuccess);
 }
 
 Participant.all = function(onError, onSuccess){
-  const sql = {command: `SELECT * FROM ${Participant.tableName};`}
+  const sql = {command: `SELECT * FROM ${TABLES.participant};`}
 
   const preOnSuccess = (result) => {
     const participants = SQL.mapResults(result, Participant.map)
@@ -52,8 +51,8 @@ Participant.all = function(onError, onSuccess){
 }
 
 Participant.deleteAll = function(){
-  const sql = {command: `DELETE FROM ${Participant.tableName};`}
-  return SQL.runSimpleCommand(sql, `Deleted all from ${Participant.tableName}`)
+  const sql = {command: `DELETE FROM ${TABLES.participant};`}
+  return SQL.runSimpleCommand(sql, `Deleted all from ${TABLES.participant}`)
 }
 
 module.exports = Participant;
