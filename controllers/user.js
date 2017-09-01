5c4a53bf4f8acc381d09bcaaedc3395ed8b4ea4c
James Cox <j-cox03@hotmail.com>
2017-09-01 22:16:00 +0100

    Made route for adding a new user

--- controllers/user.js
+++ controllers/user.js
@@ -3,16 +3,30 @@ const userRouter = new express.Router();
 const requestAuth = require("./../src/services/request_auth");
 
 const User = require("./../src/models/user");
+const onError = (error) => {console.log(error.stack)}
 
 userRouter.get('/', requestAuth, function(req, res, next){
-  const onError = (error) => {console.log(error.stack)}
   User.all(onError, (results) => {
     res.json(results)
   });
 });
 
+userRouter.post('/', requestAuth, function(req, res, next){
+  const userHash = req.body.user;
+  const newUser = new User(
+    userHash.name, userHash.avatar,
+    userHash.deviceSystem, userHash.deviceToken,
+    userHash.onlineStatus, userHash.isVisible
+  );
+
+  newUser.save().then(() => {
+    User.all(onError, (results) => {
+      res.json(newUser);
+    });
+  });
+});
+
 userRouter.get('/online', requestAuth, function(req, res, next){
-  const onError = (error) => {console.log(error.stack)}
   User.getVisiblyOnline(onError, (results) => {
     res.json(results)
   });
