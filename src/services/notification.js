eb3c027191d3206473188ab8aa6c80195270b484
James Cox <j-cox03@hotmail.com>
2017-09-01 14:58:03 +0100

    Changed constructor of notification object

--- src/services/notification.js
+++ src/services/notification.js
@@ -3,15 +3,19 @@ var FCM = require('fcm-node');
 var serverKey = require(process.env.FIREBASE_KEY); //put the generated private key path here
 var fcm = new FCM(serverKey);
 
-function Notification(userToken, notification){
+function Notification(userToken, title, body){
   this.userToken = userToken;
-  this.notification = notification;
+  this.title = title;
+  this.body = body;
 }
 
 Notification.prototype.send = function(){
   var note = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
     to: this.userToken,
-    notification: this.notification
+    notification: {
+      title: this.title,
+      body: this.body
+    }
   }
 
   fcm.send(note, function(err, response){
