9bf736be7da4b3e83068f9ac103a9b6c87efdd1c
James Cox <j-cox03@hotmail.com>
2017-09-01 22:45:15 +0100

    made changed to server.js

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.json());
 
-app.use(require("./controllers/index.js"));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.use(require("./controllers/index.js"));
