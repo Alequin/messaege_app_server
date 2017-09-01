6162d601d713c574b7b3202ed62b0790a039c68d
James Cox <j-cox03@hotmail.com>
2017-09-01 14:10:43 +0100

    moved home route to server.js to test issues

--- controllers/index.js
+++ controllers/index.js
@@ -13,13 +13,13 @@ router.get('/', requestAuth, function(req, res, next){
 });
 
 router.get('/test', requestAuth, function(req, res, next){
-  var token = "cDQhiF_mOJw:APA91bF9Tg-GmtjNPCNBjVfC08a6y5QCgJuc50RO12KEuj55x9HuqXvDV8CYCaLySi-1ZJCVW_ejBtiHyoG8Tq8wdXRUxR5YFyjB-tZ9P1hdhwvbkOMp-tAl93kkj2jMctNN3hOgYW4f";
-  var notification = {
-    title: 'Title of your push notification',
-    body: 'Body of your push notification'
-  }
-  var x = new Notification(token, notification);
-  x.send();
+  // var token = "cDQhiF_mOJw:APA91bF9Tg-GmtjNPCNBjVfC08a6y5QCgJuc50RO12KEuj55x9HuqXvDV8CYCaLySi-1ZJCVW_ejBtiHyoG8Tq8wdXRUxR5YFyjB-tZ9P1hdhwvbkOMp-tAl93kkj2jMctNN3hOgYW4f";
+  // var notification = {
+  //   title: 'Title of your push notification',
+  //   body: 'Body of your push notification'
+  // }
+  // var x = new Notification(token, notification);
+  // x.send();
   res.json({result: "sent"})
 });
 
--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser());
-app.use(require("./controllers/index.js"));
+// app.use(require("./controllers/index.js"));
+
+app.get('/', function(req, res, next){
+  res.send("Home route");
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
