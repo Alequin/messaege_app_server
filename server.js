5ab5a0ad7be1dd7855c7e5e62cac1d8ac94a7597
James Cox <j-cox03@hotmail.com>
2017-09-01 22:33:25 +0100

    Added type property to body parser

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/json' }));
 
 app.use(require("./controllers/index.js"));
 
