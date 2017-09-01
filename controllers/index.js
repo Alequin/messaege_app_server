00239873cffeffd090fa130b776608e04d9c4864
James Cox <j-cox03@hotmail.com>
2017-09-01 14:43:15 +0100

    commented out notification object

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const requestAuth = require("./../src/services/request_auth");
 
-const Notification = require("./../src/services/notification");
+// const Notification = require("./../src/services/notification");
 
 router.use("/users", require("./user"));
 router.use("/conversations", require("./conversation"));
