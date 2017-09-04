2b7693ec53b404aa290262c30853d8977b58a24f
James Cox <j-cox03@hotmail.com>
2017-09-04 14:56:48 +0100

    Made test for getting date string from date object

--- src/specs/test_date_time_handler.js
+++ src/specs/test_date_time_handler.js
@@ -11,4 +11,12 @@ describe('DateTimeHandler', function () {
     assert(DateTimeHandler.validateFormat(shortDate));
   })
 
+  it("can return date as string", () => {
+    let date = new Date(2017, 11, 5);
+
+    let expected = "2017-12-05";
+    let result = DateTimeHandler.getDateString(date);
+    assert.strictEqual(result, expected);
+  })
+
 });
