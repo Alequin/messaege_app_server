4e63f52f37f2355c7c22be92d44adacf5f66948d
James Cox <j-cox03@hotmail.com>
2017-09-04 15:33:41 +0100

    Made conversations route to create a new conversation and add the users to the participants table

--- controllers/conversation.js
+++ controllers/conversation.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const convoRouter = new express.Router();
 const requestAuth = require("./../src/services/request_auth");
+const DateTimeHandler = require("./../src/services/date_time_handler");
 
 const Convo = require("./../src/models/conversation");
+const Participant = require("./../src/models/participant");
 const ConvoInfo = require("./../src/models/conversation_info");
 
 const onError = (error) => {console.log(error.stack)}
@@ -26,8 +28,23 @@ convoRouter.get("/participants/user/:id", requestAuth, function(req, res, next){
   });
 });
 
-convoRouter.post("user/:id/other_user/:id", requestAuth, function(req, res, next){
-  const convo = new Conversation()
+convoRouter.post("/user/:id1/other_user/:id2", requestAuth, function(req, res, next){
+  let date = new Date();
+  const convo = new Convo(DateTimeHandler.getDateString(date));
+
+  const id1 = req.params.id1;
+  const id2 = req.params.id2;
+
+  convo.save().then(() => {
+    let participant1 = new Participant(id1, convo.id);
+    let participant2 = new Participant(id2, convo.id);
+
+    participant1.save().then(() => {
+      return participant2.save();
+    }).then(() => {
+      res.json({result: true});
+    });
+  });
 })
 
 module.exports = convoRouter;
