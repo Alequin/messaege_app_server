2d81fb0c5809b5aa9123e801081f258f1ddddb00
James Cox <j-cox03@hotmail.com>
2017-09-06 14:15:29 +0100

    Made route on participants controller to post a new participant

--- controllers/participants.js
+++ controllers/participants.js
@@ -2,10 +2,17 @@ const express = require('express');
 const participantsRouter = new express.Router();
 const requestAuth = require("./../src/services/request_auth");
 
+const Participant = require("./../src/models/participant");
+
 const onError = (error) => {console.log(error.stack)}
 
-participantsRouter.post('/', requestAuth, function(req, res, next){
-  
+participantsRouter.post('/user/:id1/conversation/:id2', requestAuth, function(req, res, next){
+  const userId = req.params.id1;
+  const convoId = req.params.id2;
+  const newParticipant = new Participant(userId, convoId);
+  newParticipant.save().then(() => {
+    res.json({result: "success"})
+  });
 });
 
 module.exports = participantsRouter;
